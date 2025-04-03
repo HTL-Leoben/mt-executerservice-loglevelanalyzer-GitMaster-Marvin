5b6b0d25f0401c7e5aab0ad9253269cf92a565b5
GitMaster-Marvin <211wita12@o365.htl-leoben.at>
2025-04-03 09:58:23 +0200

    Stundenende03.04
    Stundenende03.04

---
+++ MT_LogfileAnalyzer/src/main/java/LogAnalyzerTask.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+public class LogAnalyzerTask implements Callable<Map<String, Integer>> {
+
+
+    private final Path file;
+    private static final String[] LEVELS = {"TRACE", "DEBUG", "INFO", "WARN", "ERROR"};
+
+    public LogAnalyzerTask(Path file) {
+        this.file = file;
+    }
+
+
+
+    @Override
+    public Map<String, Integer> call() throws Exception {
+
+        Map<String, Integer> counts = new HashMap<>();
+
+
+        for (String level : LEVELS) {
+            counts.put(level, 0);
+            ;
+        }
+
+
+
+
+        try (BufferedReader reader = Files.newBufferedReader(file)) {
+            String line = "";
+            for (String level : LEVELS) {
+                if (line.contains(" " + level + " ")) {
+                    counts.put(level, counts.get(level) + 1);
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Fertig analysiert: " + file.getFileName() + " → " + counts);
+        return counts;
+
+    }
+}
